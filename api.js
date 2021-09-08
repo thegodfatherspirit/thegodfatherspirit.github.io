2b1ee0a2d30fd1b0b15fdb4f1ff392054c6cd93e
thegodfatherspirit <90318446+thegodfatherspirit@users.noreply.github.com>
2021-09-08 13:03:54 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 function myFunction()
 {
+console.log("enter myFunction")
 var request = new XMLHttpRequest();
 request.open('GET', 'http://worldclockapi.com/api/json/est/now', true); // YOUR EXTERNAL JSON URL     
 if (request.status >= 200 && request.status < 400) {
