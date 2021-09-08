35fd852d9da7338949ec26db64352647618e6294
thegodfatherspirit <90318446+thegodfatherspirit@users.noreply.github.com>
2021-09-08 13:09:15 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,11 @@
+function reqListener () {
+  console.log(this.responseText);
+}
+
 function myFunction()
 {
-     var request = new XMLHttpRequest();
-     request.open('GET', 'http://worldclockapi.com/api/json/est/now', true); // YOUR EXTERNAL JSON URL     
-     if (request.status >= 200 && request.status < 400) {
-              var data = JSON.parse(request.responseText);
-              alert(data);
-     }
+     var oReq = new XMLHttpRequest();
+     oReq.addEventListener("load", reqListener);
+     oReq.open("GET", "http://worldclockapi.com/api/json/est/now");
+     oReq.send();
 }
